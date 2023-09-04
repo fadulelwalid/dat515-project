7d52e7a4cfdbc0512cbf399aaeaebafd9dcf1943
abje <asbjorn.salhus@gmail.com>
2023-09-04 10:39:38 +0200

    added the dockerfiles and stuff

---
+++ docker_test/test/image/files/main.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+
+app.get('/', (req, res) => {
+  console.log('deez nutz');
+  res.send('<h1>Hello world</h1>');
+});
+
+app.get('/hei', (req, res) => {
+  console.log('deez nutz');
+  res.send('<h1>Hello world</h1>');
+});
+
+server.listen(80, () => {
+  console.log('listening on *:80');
+});
